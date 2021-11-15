9a15f1eee6e16d7d48bfd7bc9c49ecf72361e981
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-15 20:58:10 +0100

    add first example

---
+++ app/layouts.py
@@ -0,0 +1,11 @@
+from typing import (Dict,
+                    List)
+
+class layout:
+    def __init__(self, n_layout, data, disposition):
+        n_layout = n_layout
+        self.data = data
+
+    def add(self, params):
+        lay=0
+
