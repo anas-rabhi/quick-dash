4af0de9e8377a35858c8c9c0a02f2d01a40ddfef
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-19 22:36:50 +0100

    fix some bugs

--- dash_tutorial/new_tuto.py
+++ dash_tutorial/new_tuto.py
@@ -5,63 +5,28 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 data = pd.DataFrame({'a': ['c', 'd'], 'b': [20, 40]})
+import dash_html_components as html
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-        html.Div([
-            html.Div([
-                dcc.Graph(
-                    figure=px.bar(data, x='a', y='b')
-                )
-            ], style={'width': '49%', 'float': 'left', 'display': 'inline-block'}),
-            html.Div([
-                dcc.Graph(
-                    figure=px.bar(data, x='a', y='b')
-                )
-            ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})],
-
-        ),
-
-
-
-
-app.layout = html.Div([
     html.Div([
+        dcc.Graph(
+            figure=px.bar(data, x='a', y='b'))
 
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Fertility rate, total (births per woman)'
-            ),
-            dcc.RadioItems(
-                id='xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '48%', 'display': 'inline-block'}),
+    ], style={'width': '48%', 'display': 'inline-block', 'float':'right'}),
 
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
-            ),
-            dcc.RadioItems(
-                id='yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
-    ]),
 
-    dcc.Graph(id='indicator-graphic'),
+    html.Div([
+        dcc.RadioItems(
+            id='xaxis-aztype',
+            options=[{'label': 'i', 'value': 'i'}],
+            value='Linear',
+            labelStyle={'display': 'inline-block'}
+        ),
     dcc.Graph(
-        figure=px.bar(data, x='a', y='b'))
-
+                figure=px.bar(data, x='a', y='b'))
+    ], style={'width': '100%', 'display': 'inline-block'})
 ])
 
 if __name__ == '__main__':
