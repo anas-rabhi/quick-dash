8e6360d39563ca46a71af9ed73e6114cd45054f6
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-19 23:32:05 +0100

    rename directory

--- dashapp/main.py
+++ dashapp/main.py
@@ -1,4 +1,4 @@
-# Run this app with `python app.py` and
+# Run this dashapp with `python dashapp.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from app import *
+from dashapp import *
 
 # add some data
 data = pd.DataFrame({'a': ['c', 'd'], 'b': [20, 40]})
@@ -13,6 +13,7 @@ new.add_plot(where='mid', plot=px.bar, id='5d6', x='a', y='b')
 # adding one right plot
 new.add_plot(where='right', plot=px.bar, id='45', x='a', y='b')
 new.add_plot(where='left', plot=px.bar, id='45d', x='a', y='b')
+new.add_plot(where='left', plot=px.bar, id='4s5d', x='a', y='b')
 
 new.add_plot(where='mid', plot=px.bar, id='56', x='a', y='b')
 
