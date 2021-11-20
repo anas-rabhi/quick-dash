bc7fda935012b5addae845caf3c20ab7f93be02e
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-20 21:59:00 +0100

    add parameters

--- dashapp/functions.py
+++ dashapp/functions.py
@@ -2,7 +2,7 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
-
+from typing import Dict, List, Callable
 
 filters = {
     'checklist': dcc.Checklist, #    labelStyle={'display': 'inline-block'}
@@ -14,5 +14,23 @@ filters = {
     'slider': dcc.Slider
 }
 
-def define_params(type: str, var: str,**params):
-    filter = filters[type]
+
+def define_params(data, ftype: str, var: str, id: str, value: List = None,**params):
+    displayed = filters[ftype]
+
+    param = {}
+
+    if type in ['slider', 'range_slider']:
+        pass
+
+    if ftype in ['checkbox', 'checklist', 'dropdown']:
+
+        param['options'] = [{'label': i, 'value': i} for i in data[var].tolist()]
+        param['value'] = value
+        param['id'] = id
+
+        return displayed(**param, **params)
+
+    if type in ['slider', 'range_slider']:
+        pass
+
