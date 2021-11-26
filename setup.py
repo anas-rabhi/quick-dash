96c48355ba48535d057e9396334217fe2c555d0a
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-26 13:43:27 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     python_requires='>=3.7.0',
     url='https://github.com/anas-rabhi/quick-dash',
     install_requires=required,
-    packages=find_packages(include=["dashapp*", "dashapp.*"]),
+    packages=find_packages(include=["dashapp*"]),
     include_package_data=True,
     license='MIT'
 )
