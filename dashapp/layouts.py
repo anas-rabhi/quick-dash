e92aa43b6628015888c291c7f8bb3c03a6bbfff3
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-11-26 13:28:38 +0100

    fix dependency

--- dashapp/layouts.py
+++ dashapp/layouts.py
@@ -1,7 +1,6 @@
 from typing import (Dict,
                     List,
-                    Callable,
-                    Literal
+                    Callable
                     )
 import dash
 import dash_html_components as html
