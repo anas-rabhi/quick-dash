3b5635b3ebf916799f3d5c3eb4b800f96b843990
anas-rabhi <anas.rabhi.hakim@gmail.com>
2021-12-07 21:52:14 +0100

    update test file

--- test.py
+++ test.py
@@ -7,13 +7,11 @@ data = pd.DataFrame({'a': ['c', 'd'], 'b': [20, 40]})
 new = Dashboard(data)
 
 # adding one left plot
-new.add_plot(where='right', plot=px.bar, id='3', x='a', y='b')
 new.add_plot(where='mid', plot=px.bar, id='5d6', x='a', y='b')
 
 # adding one right plot
 new.add_plot(where='right', plot=px.bar, id='45', x='a', y='b')
 new.add_plot(where='left', plot=px.bar, id='45d', x='a', y='b')
-new.add_plot(where='left', plot=px.bar, id='4s5d', x='a', y='b')
 
 new.add_plot(where='mid', plot=px.bar, id='56', x='a', y='b')
 
