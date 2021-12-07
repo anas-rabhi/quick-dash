491fe7a70385a99d6fd2147d3db8c5feed4646ff
anas-rabhi <anas.rabhi.hakim@gmail.com>
2021-12-07 21:51:30 +0100

    fix imports

--- dashapp/functions.py
+++ dashapp/functions.py
@@ -1,6 +1,5 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import dcc, html
 from dash.dependencies import Input, Output
 from typing import Dict, List, Callable
 import pandas as pd
