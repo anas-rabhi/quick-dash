40c7d2bfa24ac11b5088403cfb3ee23f432a423d
anas-rabhi <anas.rabhi.hakim@gmail.com>
2021-12-09 20:42:06 +0100

    update test file

--- test.py
+++ test.py
@@ -1,18 +1,34 @@
 from dashapp import *
 import pandas as pd
 # add some data
-data = pd.DataFrame({'a': ['c', 'd'], 'b': [20, 40]})
+
+data = pd.DataFrame({
+    'Brand': ['iPhone', 'OnePlus', 'Pixel', 'Poco', 'Samsung'],
+    'Price': [2040, 1000, 500, 450, 2000],
+    'Battery': [500, 502, 520, 550, 530],
+    'RAM': [4, 0, 3, 5, 6],
+    'Rating': [70, 80, 90, 85, 70]})
 
 # initiating the dashboard class with the data as a parameter
 new = Dashboard(data)
 
 # adding one left plot
-new.add_plot(where='mid', plot=px.bar, id='5d6', x='a', y='b')
+new.add_filter(where='mid', ftype='dropdown', var='Brand', id='top_dropdown')
+new.add_plot(where='mid', plot=px.bar, id='mid_plot', x='Brand', y='Price')
 
 # adding one right plot
-new.add_plot(where='right', plot=px.bar, id='45', x='a', y='b')
-new.add_plot(where='left', plot=px.bar, id='45d', x='a', y='b')
+new.add_plot(where='right', plot=px.bar, id='right_plot', x='Brand', y='Battery')
+new.add_plot(where='left', plot=px.bar, id='left_plot', x='Brand', y='RAM')
 
-new.add_plot(where='mid', plot=px.bar, id='56', x='a', y='b')
+new.add_plot(where='mid', plot=px.bar, id='mid_plot_2', x='Brand', y='Rating')
+
+# to do :
+#new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot', 'right_plot', 'left_plot'])
+
+new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot'])
+new.add_callback(input_id=['top_dropdown'], output_id=['right_plot'])
+new.add_callback(input_id=['top_dropdown'], output_id=['left_plot'])
+new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot_2'])
+
 
 new.run_app()
