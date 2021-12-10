ccaa35c4c90efbbc3c2265420b2f270ab8c79ebb
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-12-11 00:37:28 +0100

    update demo file

--- test.py
+++ test.py
@@ -7,13 +7,17 @@ data = pd.DataFrame({
     'Price': [2040, 1000, 500, 450, 2000],
     'Battery': [500, 502, 520, 550, 530],
     'RAM': [4, 1, 3, 5, 6],
-    'Rating': [70, 80, 90, 85, 70]})
+    'Rating': [70, 80, 90, 85, 70],
+    'Design': ['Nice', 'Horrible', 'Neutral', 'Nice', 'Horrible']})
 
 # initiating the dashboard class with the data as a parameter
 new = Dashboard(data, title='Demo APP')
 
 # adding one left plot
 new.add_filter(where='mid', ftype='dropdown', var='Brand', id='top_dropdown', multi=True)
+new.add_filter(where='mid', ftype='dropdown', var='Design', id='top_dropdown_2', multi=True)
+
+
 new.add_plot(where='mid', plot=px.bar, id='mid_plot', x='Brand', y='Price', color="Battery", title='Phone prices')
 
 # adding one right plot
@@ -22,10 +26,13 @@ new.add_plot(where='left', plot=px.bar, id='left_plot', x='Brand', y='RAM', titl
 
 new.add_plot(where='mid', plot=px.bar, id='mid_plot_2', x='Brand', y='Rating', color='Price', title='Phone Rating')
 
-new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot', 'left_plot', 'right_plot', 'mid_plot_2'])
+new.add_callback(input_id=['top_dropdown', 'top_dropdown_2'], output_id=['mid_plot', 'left_plot', 'right_plot', 'mid_plot_2'])
+#new.add_callback(input_id=['top_dropdown_2'], output_id=['mid_plot', 'left_plot', 'right_plot', 'mid_plot_2'])
+
 #new.add_callback(input_id=['top_dropdown'], output_id=['right_plot'])
 #new.add_callback(input_id=['top_dropdown'], output_id=['left_plot'])
 #new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot_2'])
+#new.get_info()
 
 
 new.run_app()
