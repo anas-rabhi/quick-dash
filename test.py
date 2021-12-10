a603f67987a5964d099a7639224918bcd2a424f2
anas-r-h <anas.rabhi.hakim@gmail.com>
2021-12-11 00:04:13 +0100

    update demo file

--- test.py
+++ test.py
@@ -6,24 +6,21 @@ data = pd.DataFrame({
     'Brand': ['iPhone', 'OnePlus', 'Pixel', 'Poco', 'Samsung'],
     'Price': [2040, 1000, 500, 450, 2000],
     'Battery': [500, 502, 520, 550, 530],
-    'RAM': [4, 0, 3, 5, 6],
+    'RAM': [4, 1, 3, 5, 6],
     'Rating': [70, 80, 90, 85, 70]})
 
 # initiating the dashboard class with the data as a parameter
-new = Dashboard(data)
+new = Dashboard(data, title='Demo APP')
 
 # adding one left plot
 new.add_filter(where='mid', ftype='dropdown', var='Brand', id='top_dropdown', multi=True)
-new.add_plot(where='mid', plot=px.bar, id='mid_plot', x='Brand', y='Price')
+new.add_plot(where='mid', plot=px.bar, id='mid_plot', x='Brand', y='Price', color="Battery", title='Phone prices')
 
 # adding one right plot
-new.add_plot(where='right', plot=px.bar, id='right_plot', x='Brand', y='Battery')
-new.add_plot(where='left', plot=px.bar, id='left_plot', x='Brand', y='RAM')
+new.add_plot(where='right', plot=px.bar, id='right_plot', x='Brand', y='Battery', title='Phone battery')
+new.add_plot(where='left', plot=px.bar, id='left_plot', x='Brand', y='RAM', title='Phone RAM')
 
-new.add_plot(where='mid', plot=px.bar, id='mid_plot_2', x='Brand', y='Rating')
-
-# to do :
-#new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot', 'right_plot', 'left_plot'])
+new.add_plot(where='mid', plot=px.bar, id='mid_plot_2', x='Brand', y='Rating', color='Price', title='Phone Rating')
 
 new.add_callback(input_id=['top_dropdown'], output_id=['mid_plot', 'left_plot', 'right_plot', 'mid_plot_2'])
 #new.add_callback(input_id=['top_dropdown'], output_id=['right_plot'])
